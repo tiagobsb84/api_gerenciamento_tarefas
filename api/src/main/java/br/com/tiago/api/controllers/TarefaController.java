27108e6ede8417cc56f0f419c5e4a9e43832d8ef
Tiago Silva <tiagobsb84@gmail.com>
2023-07-02 11:01:41 -0300

    Funcionalidade de alocar pessoa na tarefa

--- api/src/main/java/br/com/tiago/api/controllers/TarefaController.java
+++ api/src/main/java/br/com/tiago/api/controllers/TarefaController.java
@@ -3,11 +3,14 @@ package br.com.tiago.api.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.tiago.api.dtos.AlocarPessoaDto;
 import br.com.tiago.api.dtos.TarefaDto;
 import br.com.tiago.api.models.Tarefa;
 import br.com.tiago.api.services.TarefaService;
@@ -25,4 +28,10 @@ public class TarefaController {
 	public ResponseEntity<Tarefa> adicinarTarefa(@RequestBody TarefaDto tarefaDto) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(tarefaService.AdicionarTarefa(tarefaDto));
 	}
+	
+	@PutMapping("/alocar/{id}")
+	public ResponseEntity<Tarefa> AlocaUmaPessoaNaTarefa(@PathVariable Long id, AlocarPessoaDto alocarPessoaDto) {
+		return ResponseEntity.status(HttpStatus.OK).body(tarefaService.AlocaUmaPessoaNaTarefa(id, alocarPessoaDto.getPessoa_id()));
+	}
+	
 }
--- api/src/main/java/br/com/tiago/api/services/TarefaService.java
+++ api/src/main/java/br/com/tiago/api/services/TarefaService.java
@@ -44,4 +44,27 @@ public class TarefaService {
 		
 		return tarefaRepository.save(tarefa);
 	}
+	
+	@Transactional
+	public Tarefa AlocaUmaPessoaNaTarefa(Long id, Long pessoa_id) {
+		Optional<Tarefa> buscandoTarefa = tarefaRepository.findById(id);
+		Optional<Pessoa> buscandoPessoa = pessoaRepository.findById(pessoa_id);
+		
+		if(!buscandoTarefa.isPresent()) {
+			throw new Error("Tarefa não encontrada");
+		}
+		
+		if(!buscandoPessoa.isPresent()) {
+			throw new Error("Pessoa não encontrada");
+		}
+		
+		if(!buscandoPessoa.get().getDepartamento().equals(buscandoPessoa.get().getDepartamento())) {
+			throw new Error("Está no mesmo departamento");
+		}
+		
+		Tarefa tarefa = buscandoTarefa.get();
+		tarefa.setPessoaAlocada(buscandoPessoa.get());
+		
+		return tarefaRepository.save(tarefa);
+	}
 }
